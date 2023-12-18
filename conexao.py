03c35e52ecf2d3b003451f81c9bd598f22e98265
AdrielLucasF <alfagundes76@gmail.com>
2023-12-18 06:44:32 -0300

    primeiro commit

--- conexao.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-class Conexao:
-    def __init__(self, db_name='academia.db'):
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
-
-    def close(self):
-        self.conn.close()
